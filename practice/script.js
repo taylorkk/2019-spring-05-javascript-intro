309afc6527e96a3b1d45edd2bc0b0ea190f41750
Meghan Knoll <megknoll@gmail.com>
2019-04-01 14:30:37 -0400

    Update script.js

--- practice/script.js
+++ practice/script.js
@@ -3,14 +3,16 @@
 
 // 2. Update the HTML to link the JQuery library
 
-// 3. Copy the click handler example from the ReadMe and copy it into your script file 
+// 3. Find the document ready example from the ReadMe and copy it into your script file 
 
-// 4. Find the menu button in the HTML. Update the click handler to select the correct button.
+// 4. Find the click handler example from the ReadMe and copy it into your script file, inside the document ready curly brackets. 
 
-// 5. We want our menu to slide in and out when we click the button. 
+// 5. Find the menu button in the HTML. Update the click handler to select the correct button.
+
+// 6. We want our menu to slide in and out when we click the button. 
 // Google for JQuery "actions" that can "slide" elements. 
 // HINT: you may also want to search for "toggle" actions.
 
-// 6. Update the action from "hide" to our new action
+// 7. Update the action from "hide" to our new action
 
-// 7.  Find the navigation div in the HTML. Update the JQuery action selector so it slides the navigation div.
+// 8.  Find the navigation div in the HTML. Update the JQuery action selector so it slides the navigation div.
